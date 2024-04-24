58db4227aad0ada8ed03013ff6653ca2f454148c
rezzadtyp <rezzadityap@gmail.com>
2024-04-24 11:11:14 +0700

    chore: initial setup

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,3 +1,14 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </div>
+  );
 };
