53a697371d5938954228d775ea490ff65fb856a9
i-nafrizaldi <ivannafrizaldi@gmail.com>
2024-04-28 01:13:59 +0700

    feat: add login and register

---
+++ apps/api/src/controllers/auth.controller.ts
@@ -0,0 +1,28 @@
+import { loginService } from "@/services/auth/login.service";
+import { registerService } from "@/services/auth/register.service";
+import { NextFunction, Request, Response } from "express";
+
+export class AuthController {
+    async registerController (req:Request,res:Response,next:NextFunction){
+        try {
+            const result = await registerService(req.body)
+
+            res.status(200).send(result)
+        } catch (error) {
+            next(error)
+            
+        }
+
+    }
+    async loginController (req:Request,res:Response,next:NextFunction){
+        try {
+            const result = await loginService(req.body)
+
+            res.status(200).send(result)
+        } catch (error) {
+            next(error)
+            
+        }
+
+    }
+}
---
+++ apps/api/src/services/auth/login.service.ts
@@ -0,0 +1,38 @@
+import { hashPassword } from "@/libs/bcrypt"
+import prisma from "@/prisma"
+import { appConfig } from "@/utils/config"
+import { User } from "@prisma/client"
+import { compare } from "bcrypt"
+import { sign } from "jsonwebtoken"
+
+
+export const loginService = async(body : Pick<User,"email"|"password">)=>{
+    try {
+        const {email,password}  = body
+
+        const user = await prisma.user.findFirst({
+            where:{email:email}
+        })
+
+        if (!user) {
+            throw new Error("Incorrect email address or password !")
+        }
+
+        const isPasswordValid = await compare(password,user.password)
+
+        if (!isPasswordValid) {
+            throw new Error("Incorrect email address or password !")
+        }
+
+        const token = sign({id:user.id},appConfig.jwtSecretKey,{expiresIn:"2h"})
+        
+        return{
+            message :"Login success !",
+            data: user,
+            token:token
+        }
+    } catch (error) {
+        throw error
+        
+    }
+}
