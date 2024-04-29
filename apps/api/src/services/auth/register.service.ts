b0e064bdf0fd9db308ef36bb0a21bd16d2b573dc
rezzadtyp <rezzadityap@gmail.com>
2024-04-29 20:10:06 +0700

    fix: tailwindcss content path error

--- apps/api/src/services/auth/register.service.ts
+++ apps/api/src/services/auth/register.service.ts
@@ -1,32 +1,30 @@
-import { hashPassword } from "@/libs/bcrypt"
-import prisma from "@/prisma"
-import { User } from "@prisma/client"
+import { hashPassword } from '@/libs/bcrypt';
+import prisma from '@/prisma';
+import { User } from '@prisma/client';
 
+export const registerService = async (body: Omit<User, 'id'>) => {
+  try {
+    const { email, password } = body;
 
-export const registerService = async(body : Omit<User,"id">)=>{
-    try {
-        const {email,password}  = body
+    const existingUser = await prisma.user.findFirst({
+      where: { email: email },
+    });
 
-        const existingUser = await prisma.user.findFirst({
-            where:{email:email}
-        })
-
-        if (existingUser) {
-            throw new Error("Email already exist!")
-        }
-
-        const hashedPassword = await hashPassword(password)
-        const referralCode = await Math.random().toString(36).substring(2,7);
-
-        const newUser = await prisma.user.create({
-            data:{...body,password:hashedPassword,referral_code:referralCode}
-        })
-        return{
-            message :"Register success !",
-            data:newUser
-        }
-    } catch (error) {
-        throw error
-        
+    if (existingUser) {
+      throw new Error('Email already exist!');
     }
-}
+
+    const hashedPassword = await hashPassword(password);
+    const referralCode = await Math.random().toString(36).substring(2, 7);
+
+    const newUser = await prisma.user.create({
+      data: { ...body, password: hashedPassword, referral_code: referralCode },
+    });
+    return {
+      message: 'Register success !',
+      data: newUser,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
